6e42af0909b02e56ead43d51499f40d33b00e02b
Sana Deghat <Sana.Deghat@gmail.com>
2025-12-19 23:11:14 -0800

    added tile map
    today i added a tile map with some very basic tiles

---
+++ scenes/enemy_soldier.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -850.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+var direction = 1 
+
+func _physics_process(delta: float) -> void:
+	
+	#adding animation 
+	#if not is_on_floor():
+	#	animated_sprite_2d.animation = "jumping"
+	if velocity.x>1 or  velocity.x < -1 :
+		animated_sprite_2d.animation = "walking"
+	else:
+		animated_sprite_2d.animation = "idle"
+
+	# Add the gravity.
+	if not is_on_floor():
+		#animated_sprite_2d.animation = "jumping"
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	if direction:
+		velocity.x = direction * SPEED * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+	if direction == -1.0:
+		animated_sprite_2d.flip_h=true
+	else :
+		animated_sprite_2d.flip_h=false
+
+
+func _on_timer_timeout() -> void:
+	direction *= -1
---
+++ scenes/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -850.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _physics_process(delta: float) -> void:
+	
+	#adding animation 
+	#if not is_on_floor():
+	#	animated_sprite_2d.animation = "jumping"
+	if velocity.x>1 or  velocity.x < -1 :
+		animated_sprite_2d.animation = "running"
+	else:
+		animated_sprite_2d.animation = "idle"
+
+	# Add the gravity.hihihihi
+	if not is_on_floor():
+		animated_sprite_2d.animation = "jumping"
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+	if direction == -1.0:
+		animated_sprite_2d.flip_h=true
+	else :
+		animated_sprite_2d.flip_h=false
