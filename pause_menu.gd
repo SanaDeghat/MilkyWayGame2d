fdcb891a91c9033eaecf98826b36e4ba79ed6b19
Sana Deghat <Sana.Deghat@gmail.com>
2026-01-12 14:22:06 -0800

    ai update
    redid the pause menu ai

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,17 +1,26 @@
 extends CanvasLayer
+var last_input_was_controller: bool = false
+@onready var animated_sprite_2d: AnimatedSprite2D = $Control/Panel/VBoxContainer/TextureRect/AnimatedSprite2D
 
 func _ready():
 	visible=false
 func _input(event):
+	if animated_sprite_2d == null:
+		return
+	if event is InputEventKey:
+		animated_sprite_2d.animation = "Keyboard"
+	elif event is InputEventJoypadButton or event is InputEventJoypadMotion :
+		animated_sprite_2d.animation = "Controller"
 	if event.is_action_pressed("pause"):
-		
 		if get_tree().paused:
 			resume()
 		else:
 			pause()
 
 func pause():
+	#get_tree().paused = true
 	get_tree().paused = true
+
 	Game.time_active = false 
 	visible = true
 
